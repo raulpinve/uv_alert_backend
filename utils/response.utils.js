01cb54d260930406abca2eca5ebbd61435e20909
Raúl Velásquez Pinto <raulvelasquezpinto@gmail.com>
2026-09-23 11:57:14 -0500

    Remove: respuestaError

--- utils/response.utils.js
+++ utils/response.utils.js
@@ -5,11 +5,3 @@ export function respuestaExitosa(res, statusCode, message, data = null) {
     data
   });
 }
-
-export function respuestaError(res, statusCode, message, error = null) {
-  return res.status(statusCode).json({
-    success: false,
-    message,
-    error
-  });
-}
